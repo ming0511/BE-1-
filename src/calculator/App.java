cf67ffea0029e92232f72a0c84ec882f038802db
ming0511 <alsrbdhkalsw@naver.com>
2025-04-30 16:36:24 +0900

    feat: 양의 정수(0 포함) 입력받기

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -1,4 +1,27 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
+
+    // Lv 1. 클래스 없이 기본적인 연산을 수행할 수 있는 계산기 만들기
+    public static void main(String[] args) {
+
+        // 1. 양의 정수(0 포함)를 입력받기
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요(양의 정수(0 포함)): ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("두 번째 숫자를 입력하세요(양의 정수(0 포함)): ");
+        int num2 = scanner.nextInt();
+
+
+        // 2. 사칙연산 기호(+, -, X, %)를 입력받기
+
+        // 3. 위에서 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력하기
+
+        // 4. 반복문을 사용하되, 반복의 종류를 알려주는 "exit" 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정하기
+
+    }
 }
