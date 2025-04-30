ca041b6a086f67165d9a981637c32a9a96707f35
ming0511 <alsrbdhkalsw@naver.com>
2025-04-30 16:38:03 +0900

    feat: 연산하고 결과값 출력하기

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -24,6 +24,35 @@ public class App {
 
 
         // 3. 위에서 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력하기
+        switch (operatorC) {
+            case '+':
+                int result = num1 + num2;
+                System.out.println("result = " + result);
+                break;
+
+            case '-':
+                int result2 = num1 - num2;
+                System.out.println("result1 = " + result2);
+                break;
+
+            case 'X':
+                int result3 = num1 * num2;
+                System.out.println("result3 = " + result3);
+                break;
+
+            case '%':
+                if (num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 " + num2 + "이 입력될 수 없습니다.");
+                } else {
+                    double result4 = (double) num1 / num2;
+                    System.out.println("result4 = " + result4);
+                    break;
+                }
+
+            default:
+                System.out.println("오류 발생");
+        }
+
 
         // 4. 반복문을 사용하되, 반복의 종류를 알려주는 "exit" 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정하기
 
