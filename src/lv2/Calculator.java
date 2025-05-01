12a02596c701bd25ea276048654df99742f12ae3
ming0511 <alsrbdhkalsw@naver.com>
2025-05-01 13:16:09 +0900

    feat: Calculator 클래스가 활용될 수 있도록 수정

--- src/lv2/Calculator.java
+++ src/lv2/Calculator.java
@@ -6,13 +6,10 @@ import java.util.List;
 // 사칙연산을 수행 후, 결과값 반환 메서드 구현 & 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 틀래스를 생성
 public class Calculator {
 
-    // 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성
-    private int num1;
-    private int num2;
-    private char operatorC;
+    // 2. 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성
     private List<Double> results = new ArrayList<>();
 
-    // 사칙연산을 수행한 후, 결과값을 반환하는 메서드 구현
+    // 1. 사칙연산을 수행한 후, 결과값을 반환하는 메서드 구현
     public double calculate(int num1, int num2, char operatorC){
         double result = 0;
 
@@ -46,6 +43,7 @@ public class Calculator {
         return result;
     }
 
+    // 3.
     // 1) 양의 정수 2개(0 포함)와 연산 기호를 매개변수로 받아 사칙연산(+, -, X, %) 기능을 수행한 후
     // 2) 결과 값을 반환하는 메서드와 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성
 
